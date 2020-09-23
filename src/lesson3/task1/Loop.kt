c3fe47f0616702805be3756b6d1ffeb263a42c78
LGodrick <godrickrus@gmail. com>
2020-09-23 12:51:52 +0300

    remake SquareBetweenExists

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -4,6 +4,7 @@ package lesson3.task1
 
 import kotlin.math.sqrt
 import kotlin.math.abs
+import kotlin.math.floor
 
 // Урок 3: циклы
 // Максимальное количество баллов = 9
@@ -122,7 +123,16 @@ fun minDivisor(n: Int): Int {
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var maxNumber = 1
+    for (i in n / 2 downTo 2) {
+        if (n % i == 0) {
+            maxNumber = i
+            break
+        }
+    }
+    return maxNumber
+}
 
 /**
  * Простая (2 балла)
@@ -140,7 +150,19 @@ fun maxDivisor(n: Int): Int = TODO()
  * Написать функцию, которая находит, сколько шагов требуется для
  * этого для какого-либо начального X > 0.
  */
-fun collatzSteps(x: Int): Int = TODO()
+fun collatzSteps(x: Int): Int {
+    var number = x
+    var count = 0
+    while (number != 1) {
+        if (number % 2 == 0) {
+            number /= 2
+        } else {
+            number = 3 * number + 1
+        }
+        count++
+    }
+    return count
+}
 
 /**
  * Средняя (3 балла)
@@ -170,7 +192,12 @@ fun lcm(m: Int, n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    for (i in 2..minOf(m, n)) {
+        if (m % i == 0 && n % i == 0) return false
+    }
+    return true
+}
 
 /**
  * Средняя (3 балла)
@@ -179,7 +206,11 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+fun squareBetweenExists(m: Int, n: Int): Boolean =
+    if (m == n) {
+        true
+    } else floor(sqrt(n.toDouble())) != floor(sqrt(m.toDouble()))
+
 
 /**
  * Средняя (3 балла)
