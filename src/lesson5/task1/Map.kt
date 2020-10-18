a52bdaaa5176e4e086fadc0b17abfb38088d9a70
Dima <goryainov_mitya@mail.ru>
2020-10-18 15:46:38 +0300

    5.5 tasks from lesson 5 are done.

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -98,7 +98,17 @@ fun buildWordSet(text: List<String>): MutableSet<String> {
  *   buildGrades(mapOf("Марат" to 3, "Семён" to 5, "Михаил" to 5))
  *     -> mapOf(5 to listOf("Семён", "Михаил"), 3 to listOf("Марат"))
  */
-fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
+fun buildGrades(grades: Map<String, Int>): MutableMap<Int, List<String>> {
+    val result = mutableMapOf<Int, List<String>>()
+    for ((key, value) in grades) {
+        if (result[value] == null) {
+            result[value] = key.split(" ")
+        } else {
+            result[value] = result[value]!! + key.split(" ")
+        }
+    }
+    return result
+}
 
 /**
  * Простая (2 балла)
